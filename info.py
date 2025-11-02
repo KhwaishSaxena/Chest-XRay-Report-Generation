9099c3b3cbbf8987631fb867544f482309076b92
Khwaish Saxena <160560474+KhwaishSaxena@users.noreply.github.com>
2025-11-02 10:17:03 +0530

    Update info.py

--- info.py
+++ info.py
@@ -3,32 +3,31 @@ import streamlit as st
 st.title("Chest X-Ray Report Generation Using Deep Learning")
 st.write("""
          ## Project Overview
-             This project — Chest X-Ray Report Generation Using Deep Learning — aims to automatically generate radiology reports from chest X-ray images.
-             It uses a CNN-LSTM model with an Attention mechanism, where: The CNN (encoder) extracts image features.
-             The LSTM with attention (decoder) generates descriptive text based on those features.
-             The system helps radiologists and healthcare professionals by providing quick, consistent, and AI-assisted 
+         - This project — Chest X-Ray Report Generation Using Deep Learning — aims to automatically generate radiology reports from chest X-ray images.
+         - It uses a CNN-LSTM model with an Attention mechanism, where: The CNN (encoder) extracts image features.
+         - The LSTM with attention (decoder) generates descriptive text based on those features.
+         - The system helps radiologists and healthcare professionals by providing quick, consistent, and AI-assisted 
             medical report generation.
             
         ---
 
         ## Objectives
-
-            To build an intelligent system that can generate textual medical reports from X-ray images.
-            To reduce manual workload and reporting time for radiologists.
-            To explore the integration of Computer Vision and Natural Language Processing in the healthcare domain.
+         - To build an intelligent system that can generate textual medical reports from X-ray images.
+         - To reduce manual workload and reporting time for radiologists.
+         - To explore the integration of Computer Vision and Natural Language Processing in the healthcare domain.
             
         ---
 
         ## Dataset Information
-            The project uses publicly available Chest X-ray datasets such as IU X-Ray (Indiana University Chest X-ray  dataset).
-            This datasets contain thousands of chest X-ray images along with their corresponding radiology reports.
+         - The project uses publicly available Chest X-ray datasets such as IU X-Ray (Indiana University Chest X-ray  dataset).
+         - This datasets contain thousands of chest X-ray images along with their corresponding radiology reports.
             
         ---
 
         ## How to Use
-            Upload a chest X-ray image (PNG format).
-            Click on “Generate Report” to let the AI model process the image.
-            The app will display a generated report describing the findings.
+        - Upload a chest X-ray image (PNG format).
+        - Click on “Generate Report” to let the AI model process the image.
+        - The app will display a generated report describing the findings.
             
         ---
         
@@ -37,6 +36,7 @@ st.write("""
          ---
 
         """)
+
 
 
 
