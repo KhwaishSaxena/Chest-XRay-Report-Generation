6933bdef59bc0c4ef01c3a48c34b24e68931560b
Khwaish Saxena <160560474+KhwaishSaxena@users.noreply.github.com>
2025-11-02 10:37:09 +0530

    Update generation.py

--- generation.py
+++ generation.py
@@ -8,10 +8,13 @@ Upload a **chest X-ray image** below and click **'Generate Report'**.
 The system will analyze the image and automatically produce a descriptive medical report.
 """)
 
+--- 
+
 uploaded_file = st.file_uploader("*Upload Chest X-Ray Image*", type=["png"])
 
 if uploaded_file is not None:
     st.image(uploaded_file, caption="Uploaded X-Ray Image successfully ", use_column_width=True)
+
 
 
 
