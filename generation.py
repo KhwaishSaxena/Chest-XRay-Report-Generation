6f043adc4767c472cc8131abeae8ebad2573839e
Khwaish Saxena <160560474+KhwaishSaxena@users.noreply.github.com>
2025-11-02 10:31:30 +0530

    Update generation.py

--- generation.py
+++ generation.py
@@ -12,4 +12,6 @@ The system will analyze the image and automatically produce a descriptive medica
 
 uploaded_file = st.file_uploader("Upload Chest X-Ray Image", type=["png"])
 
+if uploaded_file is not None:
+    st.image(uploaded_file, caption="Uploaded X-Ray Image successfully ", use_column_width=True)
 
